a803cab6dbeba0176256a82570b1cc45a84a06f0
shadeying <shadeying@gmail.com>
2019-05-21 19:07:29 +0000

    Building queries with Knex

---
+++ lookup_people_knex.js
@@ -0,0 +1,24 @@
+const knex = require("knex")({
+  client: "pg",
+  connection: require("./settings")
+});
+
+function findPeople(firstName){
+  console.log("Searching ...")
+  knex("famous_people").where("first_name", firstName).asCallback((err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(`Found ${result.length} person(s) by the name '${firstName}':`);
+
+    for(let i = 0; i < result.length; i++){
+      const person = result[i];
+      console.log(`- ${person.first_name} ${person.last_name}, born '${person.birthdate}'`);
+    }
+    knex.destroy();
+  });
+}
+
+
+const firstName = process.argv.slice(2)[0];
+findPeople(firstName);
