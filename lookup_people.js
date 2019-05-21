f2480f4e655d0dbf3e3491c3b62d81c7a56e9f04
shadeying <shadeying@gmail.com>
2019-05-21 18:34:58 +0000

    Take a single command line argument to find and output famous people

---
+++ lookup_people.js
@@ -0,0 +1,33 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+const firstName = process.argv.slice(2)[0];
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  console.log("Searching ...")
+  client.query("SELECT * FROM famous_people WHERE first_name=$1", [firstName], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(`Found ${result.rows.length} person(s) by the name '${firstName}':`);
+
+    for(let i = 0; i < result.rows.length; i++){
+      const person = result.rows[i];
+      console.log(`- ${person.first_name} ${person.last_name}, born '${person.birthdate}'`);
+    }
+
+    client.end();
+  });
+});
